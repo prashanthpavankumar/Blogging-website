b009c618236b4a5b69cffbfe107347d58c7860ac
phaniisop <phanindhrav@gmail.com>
2025-07-20 17:12:45 +0530

    Update db.php

--- blogsite/config/db.php
+++ blogsite/config/db.php
@@ -1,1 +1,15 @@
+<?php
+// Database credentials
+$host = 'localhost';
+$dbname = 'blogging_website'; // change this if you named your DB differently
+$username = 'root';
+$password = ''; // XAMPP default (no password)
 
+// Create a new MySQLi connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+?>
